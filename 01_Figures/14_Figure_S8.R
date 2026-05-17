ca3ce122ae39728aa8c4a5415c13ce4e0d930a4f
Lauren Hennelly <lmhennelly@ucdavis.edu>
2026-05-17 14:36:56 -0500

    Update 14_Figure_S8.R

--- 01_Figures/14_Figure_S8.R
+++ 01_Figures/14_Figure_S8.R
@@ -1,102 +1,64 @@
-#install.packages("ggpmisc")
+library(ggplot2)
 library(ggpmisc)
+library(patchwork)
 
-dat <- read.csv ("Wild_wolf_parturition_May15_final_2026.csv", header=TRUE)
-
-## Europe
-Europe <- subset(dat, Region=="Europe")
+# ── Data ───────────────────────────────────────────────────────
+Europe <- subset(dat, Region == "Europe")
 Europe$Region2 <- factor(Europe$Region2)
 
-my_colors <- c("Iberian" = "purple", 
-               "Europe" = "orange")
+my_colors <- c("Iberian" = "purple", "Europe" = "orange")
 
-
-#PC1
-
-ggplot(Europe, aes(x = DOY, y = PC1)) +
-  geom_point(
-    aes(fill = Region2),
-    size = 3,
-    pch = 21
-  ) +
-  stat_poly_line(
-    formula = y ~ x,
-    aes(group = 1),
-    color = "black"
-  ) +      stat_poly_eq() +
-  theme_classic() +
+# ── Individual plots ───────────────────────────────────────────
+p1 <- ggplot(Europe, aes(x = DOY, y = Lat)) +
+  geom_point(aes(fill = Region2), size = 3, pch = 21) +
+  stat_poly_line(formula = y ~ x, aes(group = 1), color = "black") +
+  stat_poly_eq(formula = y ~ x, aes(group = 1)) +
   scale_x_continuous(breaks = seq(0, 365, by = 25)) +
-  scale_fill_manual(values = my_colors)
+  scale_fill_manual(values = my_colors) +
+  labs(x = "DOY", y = "Lat") +
+  theme_classic()
 
-ggsave("2026_March10_Europe_FigureSx_PC1.pdf", width=5,height=4)
-
-#Latitude
-ggplot(Europe, aes(x = DOY, y = Lat)) +
-  geom_point(
-    aes(fill = Region2),
-    size = 3,
-    pch = 21
-  ) +
-  stat_poly_line(
-    formula = y ~ x,
-    aes(group = 1),
-    color = "black"
-  ) +
-  theme_classic() +
+p2 <- ggplot(Europe, aes(x = DOY, y = PC1)) +
+  geom_point(aes(fill = Region2), size = 3, pch = 21) +
+  stat_poly_line(formula = y ~ x, aes(group = 1), color = "black") +
+  stat_poly_eq(formula = y ~ x, aes(group = 1)) +
   scale_x_continuous(breaks = seq(0, 365, by = 25)) +
-  scale_fill_manual(values = my_colors)
-ggsave("2026_Feb10_Europe_FigureSx_Latitude_.pdf", width=5,height=4)
+  scale_fill_manual(values = my_colors) +
+  labs(x = "DOY", y = "PC1") +
+  theme_classic()
 
-#Mating date
-
-ggplot(Europe, aes(x = DOY, y = daylength_original_matingdate)) +
-  geom_point(
-    aes(fill = Region2),
-    size = 3,
-    pch = 21
-  ) +
-  stat_poly_line(
-    formula = y ~ x,
-    aes(group = 1),
-    color = "black"
-  ) +
-  theme_classic() +
+p3 <- ggplot(Europe, aes(x = DOY, y = daylength_original_matingdate)) +
+  geom_point(aes(fill = Region2), size = 3, pch = 21) +
+  stat_poly_line(formula = y ~ x, aes(group = 1), color = "black") +
+  stat_poly_eq(formula = y ~ x, aes(group = 1)) +
   scale_x_continuous(breaks = seq(0, 365, by = 25)) +
-  scale_fill_manual(values = my_colors)
+  scale_fill_manual(values = my_colors) +
+  labs(x = "DOY", y = "Daylength (mating date)") +
+  theme_classic()
 
-ggsave("2026_Feb10_FigureSx_Europe_daylength_original_matingdate.pdf", width=5,height=4)
-
-#Precipitation seasonality 
-ggplot(Europe, aes(x = DOY, y = Precipitation_Seasonality)) +
-  geom_point(
-    aes(fill = Region2),
-    size = 3,
-    pch = 21
-  ) +
-  stat_poly_line(
-    formula = y ~ x,
-    aes(group = 1),
-    color = "black"
-  ) +
-  theme_classic() +
+p4 <- ggplot(Europe, aes(x = DOY, y = Precipitation_Seasonality)) +
+  geom_point(aes(fill = Region2), size = 3, pch = 21) +
+  stat_poly_line(formula = y ~ x, aes(group = 1), color = "black") +
+  stat_poly_eq(formula = y ~ x, aes(group = 1)) +
   scale_x_continuous(breaks = seq(0, 365, by = 25)) +
-  scale_fill_manual(values = my_colors)
+  scale_fill_manual(values = my_colors) +
+  labs(x = "DOY", y = "Precipitation Seasonality") +
+  theme_classic()
 
-#Mean NDVI 
-ggplot(Europe, aes(x = DOY, y = mean_ndvi)) +
-  geom_point(
-    aes(fill = Region2),
-    size = 3,
-    pch = 21
-  ) +
-  stat_poly_line(
-    formula = y ~ x,
-    aes(group = 1),
-    color = "black"
-  ) + stat_poly_eq() +
-  theme_classic() +
+p5 <- ggplot(Europe, aes(x = DOY, y = mean_ndvi)) +
+  geom_point(aes(fill = Region2), size = 3, pch = 21) +
+  stat_poly_line(formula = y ~ x, aes(group = 1), color = "black") +
+  stat_poly_eq(formula = y ~ x, aes(group = 1)) +
   scale_x_continuous(breaks = seq(0, 365, by = 25)) +
-  scale_fill_manual(values = my_colors)
+  scale_fill_manual(values = my_colors) +
+  labs(x = "DOY", y = "Mean NDVI") +
+  theme_classic()
 
-
-ggsave("2026_May15_Europe_FigureS.pdf", width=5,height=4)
+p6 <- ggplot(Europe, aes(x = DOY, y = Annual_Precipitation)) +
+  geom_point(aes(fill = Region2), size = 3, pch = 21) +
+  stat_poly_line(formula = y ~ x, aes(group = 1), color = "black") +
+  stat_poly_eq(formula = y ~ x, aes(group = 1)) +
+  scale_x_continuous(breaks = seq(0, 365, by = 25)) +
+  scale_fill_manual(values = my_colors) +
+  labs(x = "DOY", y = "Annual Precipitation") +
+  theme_classic()
