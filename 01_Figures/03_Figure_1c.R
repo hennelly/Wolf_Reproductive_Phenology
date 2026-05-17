61019bfb2b2ea04283248d1ab29c0c9312d7594a
Lauren Hennelly <lmhennelly@ucdavis.edu>
2026-05-17 13:09:35 -0500

    Create 03_Figure_1c.R

---
+++ 01_Figures/03_Figure_1c.R
@@ -0,0 +1,16 @@
+## Figure 1C ###
+
+library(dbplyr)
+
+dat <- read.csv("2026_May15_plotting_captivevswild_lat.csv", header=TRUE)
+
+Wild <- subset(dat, Group=="wild ")
+
+p1=Wild %>% ggplot(aes(x=LatGrouping_by10, y=DOY))+  
+  geom_boxplot()+
+  geom_point(position=position_jitterdodge(jitter.width=1, dodge.width = 0), 
+             pch=21, size=2.5, aes(fill=factor(Region2)))
+p2 <- p1+scale_fill_manual(values=c("olivedrab3", "forestgreen", "orangered", "darkgoldenrod1", "#56B4E9", "gray", "purple", "gray", "white", "black", "brown")) + theme_classic()
+p2 + scale_y_continuous(breaks = round(seq(min(25), max(250), by = 25),0)) 
+
+ggsave("2026_May15_Figure1c_May5_2026.pdf", width=6,height=4)
