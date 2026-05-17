f74bc5c362c055f2e43028e30bd75e33a4f4f323
Lauren Hennelly <lmhennelly@ucdavis.edu>
2026-05-17 14:46:47 -0500

    Update 14_Figure_S7.R

--- 01_Figures/14_Figure_S7.R
+++ 01_Figures/14_Figure_S7.R
@@ -1,101 +1,87 @@
-#install.packages("ggpmisc")
+library(ggplot2)
 library(ggpmisc)
+library(patchwork)
+library(dplyr)
 
-dat <- read.csv ("Wild_wolf_parturition_May15_final_2026.csv", header=TRUE)
+# ── Load and recode ────────────────────────────────────────────
+dat <- read.csv("Wild_wolf_parturition_May15_final_2026.csv", header = TRUE)
 
-## Wolves except Indian wolves 
-not_Indian <- subset(dat, Region != "Indian")
-#PC1
-plot <- ggplot(not_Indian, aes(x=DOY, y=PC1)) + 
-          geom_point(size=3, fill="darkgoldenrod1", colour="black", pch=21) + 
-          geom_smooth(method=lm) + 
-          theme_classic() +
-          theme(
-            axis.text.x = element_text(size=14),
-            axis.text.y = element_text(size=14),
-            axis.title.x = element_text(size=16),
-            axis.title.y = element_text(size=16)
-          )
-plot2 <- plot + stat_poly_line() + stat_poly_eq()
-plot2 + scale_x_continuous(breaks = round(seq(min(0), max(365), by = 25), 0))
-ggsave("2026_May15_noIndian_FigureSx_PC1_March9_2026.pdf", width=5,height=4)
+dat$Region <- recode(dat$Region,
+                     "Middle East"  = "Southwest Asia",
+                     "NorthAmerica" = "North America")
 
-#Latitude
-plot <- ggplot(not_Indian, aes(x=DOY, y=Lat)) + 
-          geom_point(size=3, fill="darkgoldenrod1", colour="black", pch=21) + 
-          geom_smooth(method=lm) + 
-          theme_classic() +
-          theme(
-            axis.text.x = element_text(size=14),
-            axis.text.y = element_text(size=14),
-            axis.title.x = element_text(size=16),
-            axis.title.y = element_text(size=16)
-          )
-plot2 <- plot + stat_poly_line() + stat_poly_eq()
-plot2 + scale_x_continuous(breaks = round(seq(min(0), max(365), by = 25), 0))
-ggsave("2026_May15_not_Indian_Lat.pdf", width=5,height=4)
+dat <- subset(dat, Region != "Indian")
 
+# ── Color scale ────────────────────────────────────────────────
+region_colors <- c("Asia"           = "purple",
+                   "Europe"         = "olivedrab3",
+                   "Southwest Asia" = "darkgoldenrod1",
+                   "North America"  = "#56B4E9",
+                   "Tibetan"        = "gray")
 
-#Mating date
-plot <- ggplot(not_Indian, aes(x=DOY, y=daylength_original_matingdate)) + 
-          geom_point(size=3, fill="darkgoldenrod1", colour="black", pch=21) + 
-          geom_smooth(method=lm) + 
-          theme_classic() +
-          theme(
-            axis.text.x = element_text(size=14),
-            axis.text.y = element_text(size=14),
-            axis.title.x = element_text(size=16),
-            axis.title.y = element_text(size=16)
-          )
-plot2 <- plot + stat_poly_line() + stat_poly_eq()
-plot2 + scale_x_continuous(breaks = round(seq(min(0), max(365), by = 25), 0))
-ggsave("2026_May15_notIndian_daylength_original_matingdate.pdf", width=5,height=4)
+# ── Individual plots ───────────────────────────────────────────
+p1_all <- ggplot(dat, aes(x = DOY, y = Lat)) +
+  geom_point(aes(fill = Region), size = 3, pch = 21) +
+  stat_poly_line(formula = y ~ x, aes(group = 1), color = "black") +
+  stat_poly_eq(formula = y ~ x, aes(group = 1)) +
+  scale_x_continuous(breaks = seq(0, 365, by = 25)) +
+  scale_fill_manual(values = region_colors) +
+  labs(x = "DOY", y = "Lat") +
+  theme_classic()
 
+p2_all <- ggplot(dat, aes(x = DOY, y = PC1)) +
+  geom_point(aes(fill = Region), size = 3, pch = 21) +
+  stat_poly_line(formula = y ~ x, aes(group = 1), color = "black") +
+  stat_poly_eq(formula = y ~ x, aes(group = 1)) +
+  scale_x_continuous(breaks = seq(0, 365, by = 25)) +
+  scale_fill_manual(values = region_colors) +
+  labs(x = "DOY", y = "PC1") +
+  theme_classic()
 
-#Precipitation seasonality 
-plot <- ggplot(not_Indian, aes(x=DOY, y=Precipitation_Seasonality)) + 
-          geom_point(size=3, fill="darkgoldenrod1", colour="black", pch=21) + 
-          geom_smooth(method=lm) + 
-          theme_classic() +
-          theme(
-            axis.text.x = element_text(size=14),
-            axis.text.y = element_text(size=14),
-            axis.title.x = element_text(size=16),
-            axis.title.y = element_text(size=16)
-          )
-plot2 <- plot + stat_poly_line() + stat_poly_eq()
-plot2 + scale_x_continuous(breaks = round(seq(min(0), max(365), by = 25), 0))
-ggsave("2026_May15_notIndian_Precipitation_Seasonality.pdf", width=5,height=4)
+p3_all <- ggplot(dat, aes(x = DOY, y = daylength_original_matingdate)) +
+  geom_point(aes(fill = Region), size = 3, pch = 21) +
+  stat_poly_line(formula = y ~ x, aes(group = 1), color = "black") +
+  stat_poly_eq(formula = y ~ x, aes(group = 1)) +
+  scale_x_continuous(breaks = seq(0, 365, by = 25)) +
+  scale_fill_manual(values = region_colors) +
+  labs(x = "DOY", y = "Daylength (mating date)") +
+  theme_classic()
 
+p4_all <- ggplot(dat, aes(x = DOY, y = Precipitation_Seasonality)) +
+  geom_point(aes(fill = Region), size = 3, pch = 21) +
+  stat_poly_line(formula = y ~ x, aes(group = 1), color = "black") +
+  stat_poly_eq(formula = y ~ x, aes(group = 1)) +
+  scale_x_continuous(breaks = seq(0, 365, by = 25)) +
+  scale_fill_manual(values = region_colors) +
+  labs(x = "DOY", y = "Precipitation Seasonality") +
+  theme_classic()
 
-#Annual_Precipitation 
-plot <- ggplot(not_Indian, aes(x=DOY, y=Annual_Precipitation)) + 
-          geom_point(size=3, fill="darkgoldenrod1", colour="black", pch=21) + 
-          geom_smooth(method=lm) + 
-          theme_classic() +
-          theme(
-            axis.text.x = element_text(size=14),
-            axis.text.y = element_text(size=14),
-            axis.title.x = element_text(size=16),
-            axis.title.y = element_text(size=16)
-          )
-plot2 <- plot + stat_poly_line() + stat_poly_eq()
-plot2 + scale_x_continuous(breaks = round(seq(min(0), max(365), by = 25), 0))
+p5_all <- ggplot(dat, aes(x = DOY, y = mean_ndvi)) +
+  geom_point(aes(fill = Region), size = 3, pch = 21) +
+  stat_poly_line(formula = y ~ x, aes(group = 1), color = "black") +
+  stat_poly_eq(formula = y ~ x, aes(group = 1)) +
+  scale_x_continuous(breaks = seq(0, 365, by = 25)) +
+  scale_fill_manual(values = region_colors) +
+  labs(x = "DOY", y = "Mean NDVI") +
+  theme_classic()
 
-ggsave("2026_May15_notIndian_annual_precipitation.pdf", width=5,height=4)
+p6_all <- ggplot(dat, aes(x = DOY, y = Annual_Precipitation)) +
+  geom_point(aes(fill = Region), size = 3, pch = 21) +
+  stat_poly_line(formula = y ~ x, aes(group = 1), color = "black") +
+  stat_poly_eq(formula = y ~ x, aes(group = 1)) +
+  scale_x_continuous(breaks = seq(0, 365, by = 25)) +
+  scale_fill_manual(values = region_colors) +
+  labs(x = "DOY", y = "Annual Precipitation") +
+  theme_classic()
 
-#mean_ndvi 
-plot <- ggplot(not_Indian, aes(x=DOY, y=mean_ndvi)) + 
-          geom_point(size=3, fill="darkgoldenrod1", colour="black", pch=21) + 
-          geom_smooth(method=lm) + 
-          theme_classic() +
-          theme(
-            axis.text.x = element_text(size=14),
-            axis.text.y = element_text(size=14),
-            axis.title.x = element_text(size=16),
-            axis.title.y = element_text(size=16)
-          )
-plot2 <- plot + stat_poly_line() + stat_poly_eq()
-plot2 + scale_x_continuous(breaks = round(seq(min(0), max(365), by = 25), 0))
-ggsave("2026_May15_notIndian_mean_ndvi.pdf", width=5,height=4)
+# ── Combine ────────────────────────────────────────────────────
+combined_all <- (p1_all | p2_all) / (p3_all | p4_all) / (p5_all | p6_all) +
+  plot_layout(guides = "collect") &
+  theme(legend.position = "bottom",
+        legend.title = element_blank(),
+        legend.text = element_text(size = 14))
 
+print(combined_all)
+
+ggsave("May17_2026_AllWolves_noIndian_panel.pdf",
+       plot = combined_all, width = 10, height = 12, dpi = 300)
