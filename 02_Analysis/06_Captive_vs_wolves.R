44add7d38721276c55b571f77bc66da709b12ebb
Lauren Hennelly <lmhennelly@ucdavis.edu>
2026-05-17 15:42:13 -0500

    Create 06_Captive_vs_wolves.R

---
+++ 02_Analysis/06_Captive_vs_wolves.R
@@ -0,0 +1,26 @@
+dat <- read.csv("May15_2026_comparison_wildvscaptive_final.csv", header=TRUE)
+
+Captive <- subset(dat, Captive_vs_Wild=="Captive")
+mean(Captive$adjusted_DOBirthYOD)
+#183.4383 May 2rd
+sd(Captive$adjusted_DOBirthYOD)
+#40.52895
+Wild <- subset(dat, Captive_vs_Wild=="Wild")
+mean(Wild$adjusted_DOBirthYOD)
+#170.229
+sd(Wild$adjusted_DOBirthYOD)
+#35.07582 with YELLOWSTONE
+## Welch's t-test
+dat <- read.csv("May14_2026_WildvsCaptive_Ttest_Yellowstone.csv", header=TRUE)
+t.test(dat$Captive_adjusted_DOBirthYOD, dat$Wild_adjustedDOY, var.equal = FALSE) 
+
+	Welch Two Sample t-test
+
+data:  dat$Captive_adjusted_DOBirthYOD and dat$Wild_adjustedDOY
+t = 6.1689, df = 1270.1, p-value = 9.225e-10
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+  8.939315 17.276359
+sample estimates:
+mean of x mean of y 
+ 183.4383  170.3305 
